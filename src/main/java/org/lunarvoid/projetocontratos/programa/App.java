c752445c04965c3210d60d292f21826462af62b2
Pedro Maia <pedrodrm250@gmail.com>
2025-11-24 22:01:50 -0300

    testando git hub para trabalhar em multiplos dispositivos

---
+++ src/main/java/org/lunarvoid/projetocontratos/programa/App.java
@@ -0,0 +1,33 @@
+package org.lunarvoid.projetocontratos.programa;
+
+import org.lunarvoid.projetocontratos.entidades.Contrato;
+import org.lunarvoid.projetocontratos.entidades.Parcela;
+import org.lunarvoid.projetocontratos.repositores.ContratoRepositor;
+import org.lunarvoid.projetocontratos.repositores.ParcelaRepositor;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Entre com os dados do contrato");
+            System.out.print("Digite o numero do contrato: ");
+            String numero = sc.next();
+            System.out.print("Entre com o valor do contrato: ");
+            Double valor = sc.nextDouble();
+            System.out.print("Entre com a quantidade de parcelas: ");
+            Integer qtd = sc.nextInt();
+            
+            Contrato ct = new Contrato(numero, LocalDate.now(), valor);
+            
+            new ContratoRepositor().addContrato(ct, qtd);
+            List<Parcela> parcelas = new ParcelaRepositor().getParcelasBanco(ct);
+
+            for (Parcela p : parcelas){
+                System.out.println(p);
+            }
+            
+        }
+    }
+}
